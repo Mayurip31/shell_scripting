d21d24c5ed69c73d1dd62b9ee4a472d90118c498
Mayurip31 <mayuripawar6196@gmail.com>
2024-03-01 16:55:04 +0530

    first file of shell

---
+++ Task.sh
@@ -0,0 +1,25 @@
+!/bin/bash
+
+# Create a directory
+mkdir Task
+
+# Change into the created directory
+cd Task
+
+# create file
+
+touch file{1.3}.txt
+
+#  Add content in files using redirectors
+
+echo "First file" > file1.txt
+echo "second file" > file2.txt
+echo "third file" > file3.txt
+
+# Display the contents of all three files
+cat file1.txt
+cat file2.txt
+cat file3.txt
+
+## To run script 
+bash file.sh
